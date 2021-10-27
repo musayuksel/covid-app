075e6d966530c576fb46561c60242945451b47ae
S10 <30575036+S10MC2015@users.noreply.github.com>
2021-10-27 20:46:29 +0100

    fix #5 and remove the commented out code

--- src/CreateLine.js
+++ src/CreateLine.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-// import OneBar from "./OneBar";
 
 export default function CreateLine({
   data,
@@ -7,7 +6,6 @@ export default function CreateLine({
   SVGWidth,
 }) {
   const maxCase = Math.max(...data.map((e) => e.newCases));
-  // const barWith = SVGWidth / data.length; // if there is more data, barwith will be small
 
   const points = data.map(
     (dataPoint, i) =>
@@ -17,25 +15,8 @@ export default function CreateLine({
   );
 
   console.table(points);
-  // console.log(data);
   return (
     <>
-      {/* {data.map((day, i) => {
-        const barHeight =
-          (day.newCases * graphHeight * 0.8) / maxCase; //barHeight will be relative to the data. the highest "newCase" value will be 80% of table hight
-        return (
-          <OneBar
-            key={i}
-            day={day}
-            barHeight={barHeight}
-            i={i}
-            barWith={barWith}
-          />
-
-
-        );
-      })} */}
-
       <polyline
         points={`0, 0 ${points} ${data.length}, 0`}
         stroke="black"
