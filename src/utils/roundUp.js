0bc504e31638e7e8c443c7f6966abd24b12384a5
Michael Shields <73437037+mike-shields-dev@users.noreply.github.com>
2021-11-09 16:58:41 +0000

    Create roundUp.js
    This function rounds up a number but takes into consideration the magnitude of the number. It needs tweaking.

---
+++ src/utils/roundUp.js
@@ -0,0 +1,14 @@
+function roundUp(number) {  
+  const baseLog10 = 
+    Math.ceil(
+      Math.log(number) / Math.log(10)
+    )
+
+  const divisor = 10 ** (baseLog10 - 1) 
+
+  return (
+    Math.ceil(number * 2 / divisor) / 2
+  ) * divisor 
+}
+
+export default roundUp
